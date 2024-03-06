4e91e765ac5501466579fa4b0fa1cedfa892108c
Seongae Kim <30529101+abc4571998@users.noreply.github.com>
2024-03-06 13:05:36 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,20 +1,3 @@
-import re
-
 input_str = input()
-input_list = re.findall(r'\d+|[-+]', input_str)
-sum = 0
-
-is_minus = False
-for i in input_list:
-  if (i.isdigit()):
-    if is_minus == True:
-      sum -= int(i)
-    else:
-      sum += int(i)
-  else:
-    if i == '-':
-      is_minus = True
-    elif i == '+' and is_minus == False:
-      is_minus = False
-  
-print(sum)
+input_str = [sum(map(int, x.split('+'))) for x in input_str.split('-')]  
+print(input_str[0] - sum(input_str[1:]))
