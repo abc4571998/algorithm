61fbbe24507bffe4c2cf9ac659b1718eee227ba5
Seongae Kim <30529101+abc4571998@users.noreply.github.com>
2025-04-25 17:19:43 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,26 +1,25 @@
 from collections import deque
 
-flame = []
 n, m = map(int, input().split())
+arr = []
+
 for _ in range(n):
-  flame.append([int(x) for x in input()])
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
+    arr.append(list(map(int, input())))
 
-def bfs(graph, x, y):
-  queue = deque()
-  queue.append((x, y))
-  while queue:
-      x, y = queue.popleft()
-      for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if nx < 0 or nx >= n or ny < 0 or ny >= m or graph[nx][ny] == 0:
-          continue
-        if graph[nx][ny] == 1:
-          graph[nx][ny] = graph[x][y] + 1
-          queue.append((nx, ny))
-  return graph[n-1][m-1]
+def bfs(arr, x, y):
+    queue = deque([(x, y)])
+    
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
 
-print(bfs(flame, 0, 0))
+    while queue: 
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and arr[nx][ny] == 1:
+                arr[nx][ny] = arr[x][y] + 1
+                queue.append((nx, ny))
 
+bfs(arr, 0, 0)
+print(arr[n-1][m-1])
